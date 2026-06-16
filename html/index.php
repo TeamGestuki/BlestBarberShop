a6b939ccf0b046944b97a2f4585a040f0015e84a
juanchi-vicino <juancruzargentina05@gmail.com>
2026-06-16 19:52:13 -0300

    Add favicon link to index.php
    Added favicon link to the HTML head section.

--- html/index.php
+++ html/index.php
@@ -22,7 +22,8 @@ try {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Blest Barber Shop</title>
+  <title>Blest Barber Shop</title> 
+  <link rel="icon" type="image/png" href="../img/logo.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
